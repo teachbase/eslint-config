e5d5e68e7b1c739b8b2486c7dcd9623db0ef10d8
Alexander Telyatnikov <labriko@yandex.ru>
2018-06-28 13:04:39 +0300

    feat: Add flow rules

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   extends: ['airbnb'],
-  plugins: ['compat'],
+  plugins: ['compat', 'flowtype'],
   env: {
     browser: true,
     es6: true,
@@ -23,7 +23,18 @@ module.exports = {
     'no-empty': ['error', {
       allowEmptyCatch: true,
     }],
-    'max-len': ['error', 120, 2],
+    "operator-linebreak": ["error", "after"],
+    "react/destructuring-assignment": "off",
+    "react/button-has-type": "off", // https://github.com/yannickcr/eslint-plugin-react/issues/1846
+    "lines-between-class-members": ["error", "always", { "exceptAfterSingleLine": true }],
+    "react/jsx-one-expression-per-line": "off",
+    "max-len": ["error", 120, 2, {
+      "ignoreUrls": true,
+      "ignoreComments": false,
+      "ignoreRegExpLiterals": false,
+      "ignoreStrings": false,
+      "ignoreTemplateLiterals": false
+    }],
     'object-curly-newline': ['error', {
       ObjectExpression: {
         minProperties: 4,
@@ -92,6 +103,56 @@ module.exports = {
     }],
     'react/jsx-filename-extension': ['error', {
       extensions: ['.js'],
-    }]
+    }],
+    "flowtype/boolean-style": [
+      "error",
+      "boolean"
+    ],
+    "flowtype/define-flow-type": "error",
+    "flowtype/delimiter-dangle": [
+      "error",
+      "always-multiline"
+    ],
+    "flowtype/generic-spacing": [
+      "error",
+      "never"
+    ],
+    "flowtype/no-primitive-constructor-types": "error",
+    "flowtype/no-types-missing-file-annotation": "error",
+    "flowtype/object-type-delimiter": [
+      "error",
+      "comma"
+    ],
+    "flowtype/no-weak-types": "error",
+    "flowtype/require-parameter-type": "error",
+    "flowtype/require-return-type": [
+      "error",
+      "always",
+      {
+        "annotateUndefined": "never"
+      }
+    ],
+    "flowtype/require-valid-file-annotation": ["error", "never", { "annotationStyle": "block" }],
+    "flowtype/semi": [
+      "error",
+      "always"
+    ],
+    "flowtype/space-after-type-colon": [
+      "error",
+      "always"
+    ],
+    "flowtype/space-before-generic-bracket": [
+      "error",
+      "never"
+    ],
+    "flowtype/space-before-type-colon": [
+      "error",
+      "never"
+    ],
+    "flowtype/union-intersection-spacing": [
+      "error",
+      "always"
+    ],
+    "flowtype/use-flow-type": "warn",
   },
 };
